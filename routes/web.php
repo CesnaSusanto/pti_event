1b07a1e27a8a2b2e4705564d83c99988fca0f54a
FaliqHusnan <faliqhusnan12@gmail.com>
2025-01-15 23:45:52 +0700

    Menambahkan fitur authentication dan pisah page admin&user

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('user.dashboard');
@@ -18,3 +19,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// Route::get('admin/dashboard', [HomeController::class, 'index']);
+Route::get('admin/dashboard', [HomeController::class, 'index'])->middleware(['auth', 'admin']);
