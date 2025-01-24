c9cd3b024134018ac12f4f2e145a742eedf3f775
leone08 <wanda.gustianingsih99@gmail.com>
2025-01-24 14:18:27 +0700

    Merge branch 'new_dashboard' of https://github.com/CesnaSusanto/pti_event into wanda_branch

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('user.dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
